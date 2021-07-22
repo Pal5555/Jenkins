82fc09f4047a7d52b315daab38290cbd9344d15a
Oz9595 <ozoda.imomkulova@gmail.com>
2021-07-21 21:19:49 -0400

    Batch 9

---
+++ src/com/collection/Main.java
@@ -0,0 +1,14 @@
+package com.collection;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for(int i=1; i<5; i++) {
+            System.out.println("Batch 9 is great");
+            Thread.sleep(5000);
+        }
+    }
+
+
+}
