e8440cd038cc8388a291f4cadc05614e4b40db81
Oz9595 <ozoda.imomkulova@gmail.com>
2021-07-22 11:19:01 -0400

    2nd commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for(int i=1; i<5; i++) {
+        for(int i=1; i<6; i++) {
             System.out.println("Batch 9 is great");
             Thread.sleep(5000);
         }
